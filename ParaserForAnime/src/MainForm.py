5af3ee68976629aa5927a09c87a74543426d635e
FishLai <38682183+FishLai@users.noreply.github.com>
2018-08-29 17:28:56 +0800

    add table header

--- ParaserForAnime/src/MainForm.py
+++ ParaserForAnime/src/MainForm.py
@@ -7,6 +7,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QMainWindow, QApplication
 
 from Frame.interface import Ui_Form
+from PyQt5.Qt import QStandardItem
 
 class MainForm(QMainWindow):
     '''
@@ -19,6 +20,21 @@ class MainForm(QMainWindow):
         super(MainForm, self).__init__(parent)
         self.ui = Ui_Form()
         self.ui.setupUi(self)
+        ui = self.ui
+        
+        ui.Btn_Re.clicked.connect(self.refreshEvent)
+        self.tableView_set()
+    def tableView_set(self):
+        self.model_tb = QtGui.QStandardItemModel()
+        m_t = self.model_tb
+        tb_h = ["Anime", "episode", "isDownload"]
+        m_t.setHorizontalHeaderLabels(tb_h)
+        table = self.ui.tableView
+        table.setModel(m_t)
+        
+        
+    def refreshEvent(self, event):
+        print("hi")
         
 if __name__ == '__main__':
     import sys
